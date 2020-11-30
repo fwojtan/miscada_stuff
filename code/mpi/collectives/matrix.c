5bd46609e4e2d0811a3e5698ea7974754331a8b4
fwojtan <finlaywojtan@hotmail.co.uk>
2020-11-30 13:33:26 +0000

    Add work for matrix collectives exercise

--- code/mpi/collectives/matrix.c
+++ code/mpi/collectives/matrix.c
@@ -5,17 +5,60 @@
 static void print_matrix(const double *A, int n, MPI_Comm comm)
 {
   /* Implement printing of A by gathering to rank 0 and printing there. */
+  int size, rank;
+  MPI_Comm_size(comm, &size);
+  MPI_Comm_rank(comm, &rank);
+  const double *sendbuf = A;
+  double *recvbuf=NULL;
+  if (rank == 0) {
+    recvbuf = malloc(size*n*sizeof(double));
+  }
+  MPI_Gather(sendbuf, n, MPI_DOUBLE, recvbuf, n, MPI_DOUBLE, 0, comm);
+  if (rank == 0) {
+    for (int row = 0; row < size; row++){
+      for (int val = 0; val < n; val++){
+        int index = row * n + val;
+        printf("%.2f\t", recvbuf[index]);
+      };
+      printf("\n");
+    }
+  }
 }
 
 static void transpose_matrix(const double *A, double *AT, int n, MPI_Comm comm)
 {
-  /* Implement AT <- transpose(A) (MPI_Alltoall may be helpful) */
+  /* Implement AT <- transpose(A) (MPI_Alltoall may be helpful) */ 
+  MPI_Alltoall(A, 1, MPI_DOUBLE, AT, 1, MPI_DOUBLE, comm);
+
+  // Note to self: the send/recv count in this instance is the count to/from
+  // EACH process, not overall...
+
 }
 
 static void matrix_vector_product(const double *A, const double *x, double *y, int n, MPI_Comm comm)
 {
   /* Implement y <- Ax, it might be useful to tranpose A first (think
      about the data distribution). */
+  double *AT = NULL;
+  double *ATT=NULL;
+
+  int rank, size;
+  MPI_Comm_rank(comm, &rank);
+  MPI_Comm_size(comm, &size);
+
+  AT = malloc(4*size * sizeof(*AT));
+  ATT = malloc(4*size * sizeof(*ATT));
+
+  transpose_matrix(A, AT, size, comm);
+  for (int i=0; i<n; i++){
+    AT[i] *= x[0];
+  }
+  transpose_matrix(AT, ATT, size, comm);
+  for (int i=0; i<n; i++){
+    y[0] += ATT[i];
+  }
+  free(AT);
+  free(ATT);
 }
 
 static void print_vector(const double *x, MPI_Comm comm)
